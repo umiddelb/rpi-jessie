16138fa90349f2960b9ad68728b85c0ad0d56b4d
Uli Middelberg <uli@middelberg.de>
2015-06-21 21:04:18 +0200

    Create make_kernel.sh

---
+++ make_kernel.sh
@@ -0,0 +1,8 @@
+export KERNEL=kernel
+make clean
+make bcmrpi_defconfig
+make -j 4 zImage modules dtbs
+sudo make modules_install
+sudo cp arch/arm/boot/dts/*.dtb /boot/
+sudo cp arch/arm/boot/dts/overlays/*.dtb* /boot/overlays/
+sudo scripts/mkknlimg arch/arm/boot/zImage /boot/$KERNEL.img
